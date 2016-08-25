e38e773a8a0582d05e856d65502230970d65e605
fcalmard <fcalemard@gmail.com>
2016-08-25 12:23:18 +0200

    ok q

--- app/src/main/java/acquisti/ouccelo/free/fr/acquisti/acquisti/MainActivity.java
+++ app/src/main/java/acquisti/ouccelo/free/fr/acquisti/acquisti/MainActivity.java
@@ -69,14 +69,6 @@ public class MainActivity extends AppCompatActivity
 
         final Context context = this;
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
         /*
         selection liste de course
          */
